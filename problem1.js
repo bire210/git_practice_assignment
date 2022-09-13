8651b11ff521b9b0413a1856047de82c8142c5d5
Birendra <divyabiru27@gmail.com>
2022-09-13 12:59:07 +0530

    if logic is wrong, you have to correct this

---
+++ problem1.js
@@ -0,0 +1,13 @@
+// check prime or not
+let num=23;
+let c=0;
+for(let i=1;i<=num;++i){
+    if(num%i==5){
+        c--;
+    }
+}
+if(c==2){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
